23bca1137e768aaf78801187e12a67837bed741d
Roman Lubij <roman.lubij@gmail.com>
2020-02-14 15:51:08 +0100

    aaa removed the console.log

--- src/events/discord/msgRecieved.js
+++ src/events/discord/msgRecieved.js
@@ -34,7 +34,6 @@ module.exports = (client, msg) => {
   const options = {
     caption: ` _${getNick(msg)}_: ${content || ``}`,
     parse_mode: `markdown` }
-  console.log(options)
   if (attachments.size !== 0) {
     attachments.map(file => {
       client.tgBot.sendDocument(groupID, file.url, options)
